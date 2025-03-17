850363b3d63f92acfd759107b16e8373acfa951e
Noel Kujur <lovenoelkujur@gmail.com>
2025-03-17 21:55:47 +0530

    Course Purchase updated

--- controllers/coursePurchaseController.js
+++ controllers/coursePurchaseController.js
@@ -47,8 +47,8 @@ const createCheckoutSession = async(req, res) => {
               },
             ],
             mode: "payment",
-            success_url: `process.env.FRONTEND_URL/course-progress/${courseId}`, // once payment successful redirect to course progress page
-            cancel_url: `process.env.FRONTEND_URL/course-detail/${courseId}`,
+            success_url: `https://lms-frontend-blond-psi.vercel.app/course-progress/${courseId}`, // once payment successful redirect to course progress page
+            cancel_url: `https://lms-frontend-blond-psi.vercel.app/course-detail/${courseId}`,
             metadata: {
               courseId: courseId,
               userId: userId,
